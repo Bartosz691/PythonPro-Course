2a73b859b0a8864290d24053f1d70b6caa19ed86
Bartosz Wypych <ccgalerty789@gmail.com>
2026-04-06 15:45:45 +0200

    task6
    converting integer number to binary and hexadecimal system

---
+++ homework/lesson1/task6_converter.py
@@ -0,0 +1,8 @@
+integer = int(input("Podaj liczbę dziesiętną: "))
+
+binary = bin(integer)
+
+hexadecimal = hex(integer)
+
+print('Twoja liczba w systemie binarnym: ',binary)
+print('Twoja liczab w systemie hexadecymalnym: ',hexadecimal)
