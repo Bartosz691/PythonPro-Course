6f630129576797b45cfe09b64e9120682494ab4d
Bartosz Wypych <ccgalerty789@gmail.com>
2026-04-06 15:31:05 +0200

    Create task4_area.py

---
+++ homework/lesson1/task4_area.py
@@ -0,0 +1,6 @@
+length_prost = float(input("Podaj długość prostokąta: "))
+width_prost = float(input("Podaj szerokość prosotokąta: "))
+
+result  = length_prost * width_prost
+
+print('Pole prosotkąta wynosi: ', result)
