646460c8cd628b1d4714c5a122f56b6a1584b944
Bartosz Wypych <ccgalerty789@gmail.com>
2026-05-01 20:08:27 +0200

    lesson9

--- homework/lesson9/task8.py
+++ homework/lesson9/task8.py
@@ -1,16 +1,19 @@
 import json
 
-nazwa_pliku = input("Podaj nazwę pliku, który chcesz otworzyć: ")
-fraza = input("Podaj frazę do wyszukiwania: ")
+logs_path = r"D:\PythonPro-Course\homework\lesson9\logs.csv"
+keyword = input("Podaj słowo klucz do wyszukania w logach: ")
 
-plik_wejsciowy = nazwa_pliku + ".txt"
-plik_wyjsciowy = "wyniki_wyszukiwania.txt"
-
-with open(plik_wejsciowy, "r", encoding="utf-8") as f:
-    
-      with open(plik_wyjsciowy, "w", encoding="utf-8") as f_out:
-
-   
-        for linia in f:
-           if fraza in linia:
-            f_out.write(linia)
+def write_keylog(line: str, keylog_filepath: str | Path = "keylogs.csv"):
+    if not line.endswith('\n'):
+        line+='\n'
+    with open(keylog_filepath, 'w', encoding='utf8') as klfp:
+        klfp.write(line)
+        
+    with open(keylog_filepath, 'r', encoding='utf-8') as klfp:
+        klfp.write(line)
+        
+with open(logs_path, 'r', encoding='utf-8') as lfp:
+    for line in lfp:
+        
+        if keyword in line:
+            write_keylog(line)
