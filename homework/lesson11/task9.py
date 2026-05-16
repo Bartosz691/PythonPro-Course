c4174382c81ac6962d7b997705839320054f3748
Bartosz Wypych <ccgalerty789@gmail.com>
2026-05-17 00:01:10 +0200

    lesson11
    examining if password and e-mail adress is correct

--- homework/lesson11/task9.py
+++ homework/lesson11/task9.py
@@ -6,7 +6,7 @@ class RejestracjaUzytkownika(Exception):
     def sprawdz_email(email):
         try:
             
-            if not isinstance(email: str):
+            if not isinstance(email, str):
                 raise TypeError("Wprowadzona wartość nie jest tekstem. ")
             
             if "@" not in email:
@@ -14,3 +14,25 @@ class RejestracjaUzytkownika(Exception):
             
             print("E-mail jest poprawny")
             return True
+        
+        except(TypeError, ValueError) as blad:
+            print(f"Błąd: {blad}")
+            return False
+
+    def sprawdz_haslo(haslo: str):
+        try:
+           if len(haslo) < 8:
+              raise ValueError("Hasło jest za krókie min. 8 znaków")
+           
+           print("Hasło jest poprawne. ")
+           return True
+       
+        except (TypeError, ValueError) as blad:
+            print(f"Błąd: {blad}")
+            return False
+        
+    email = str(input("Podaj adres e-mail: "))
+    haslo = str(input ("Podaj hasło: "))
+    
+    print(sprawdz_email(email))
+    print(sprawdz_haslo(haslo))
