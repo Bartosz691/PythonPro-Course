669ef2f29c982c40d9efce948debde70b41b6d7d
Bartosz Wypych <ccgalerty789@gmail.com>
2026-05-17 23:13:27 +0200

    lesson12

---
+++ homework/lesson12/task8.py
@@ -0,0 +1,37 @@
+
+def dzialanie(a, b, znak):
+    if znak == "+":
+        return a + b
+    elif znak == "-":
+        return a - b
+    elif znak == "/":
+        return a/b
+    elif znak == "*":
+        return  a * b
+    else:
+        return("nie obsługiwnay znak ")
+        
+while True:
+    try:
+        tekst = input("Podaj pierwszą liczbę lub 'q' aby wyjść: ")
+        
+        if tekst == "q":
+            break
+        a = float(tekst)
+        b = float(input("Podaj drugą liczbę: "))
+        znak = input("Podaj działanie (+, -, *, /): ")
+        
+        wynik = dzialanie(a, b, znak)
+    
+    except ValueError:
+        print("Błąd: Podano nieprawidłową wartość. ")
+    
+    except ZeroDivisionError:
+          print("Błąd: Nie można dzielić przez zero. ")
+    
+    else: 
+          
+          print("Wynik: ", wynik) 
+          
+    finally:
+         print("Koniec obliczeń.\n")        
