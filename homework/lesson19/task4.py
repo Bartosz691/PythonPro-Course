990590e3b1ce3187c016dd6093d1a6a0cf908198
Bartosz Wypych <ccgalerty789@gmail.com>
2026-06-05 20:27:29 +0200

    lesson

---
+++ homework/lesson19/task4.py
@@ -0,0 +1,13 @@
+# Database
+# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'bart',
+#        'USER': 'admin',
+#        'PASSWORD': 'tajne_haslo123',
+#        'HOST': 'localhost',
+#        'PORT': '5432',
+#    }
+# }
