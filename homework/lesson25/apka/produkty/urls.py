18431dd343b2966038af3475369200913be3f268
Bartosz Wypych <ccgalerty789@gmail.com>
2026-07-10 20:15:00 +0200

    lesson25

--- homework/lesson25/apka/produkty/urls.py
+++ homework/lesson25/apka/produkty/urls.py
@@ -1,8 +1,15 @@
+
 from django.urls import path
 from rest_framework import routers
 
-from .views import ProduktViewSet, NoteViewSet, set_name_view, hello_view
 
+from .views import (
+    ProduktViewSet,
+    NoteViewSet,
+    set_name_view,
+    hello_view,
+    calculate_view
+)
 
 router = routers.DefaultRouter()
 router.register(r"produkty", ProduktViewSet)
@@ -11,6 +18,7 @@ router.register(r"notes", NoteViewSet)
 urlpatterns = [
     path("set-name/", set_name_view),
     path("hello/", hello_view),
+    path("calculate/", calculate_view),
 ]
 
 urlpatterns += router.urls
