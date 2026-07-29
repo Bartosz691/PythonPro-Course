4e0cbc4114f6609204615d8e86b7d7cc26c77f4f
Bartosz Wypych <ccgalerty789@gmail.com>
2026-07-30 01:55:35 +0200

    lesson26

--- homework/lesson26/config/settings.py
+++ homework/lesson26/config/settings.py
@@ -15,6 +15,11 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=10),
+}
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
